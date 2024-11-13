f0636df0089bac28ac2a1be25d8c1e04382c1606
astridpradnya12 <astridpradnya12@gmail.com>
2024-11-13 09:55:53 +0800

    Add files via upload

---
+++ PRATIKUM/utspakhuma/UTSPemrogramanWeb/script.js
@@ -0,0 +1,22 @@
+document.getElementById("fotm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Untuk mencegah ada kolom yang kosong
+  if (!name || !email || !password) {
+    alert("Harap Mengisi kolom!!!");
+    return;
+  }
+
+  // Untuk memastikan alamat email benar
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    alert("Alamat email tidak valid!");
+    return;
+  }
+
+  alert("Formulir berhasil dikirim!");
+});
